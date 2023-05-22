a13027d4e02a9a5543271c293470fa4bbbd9f005
Pawan Singh <pawan.singh@leadschool.in>
2023-05-22 11:37:35 +0530

    deployment fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,10 @@
 import { createTheme, ThemeProvider } from "@mui/material/styles"
 import React, { useEffect, useState } from "react"
-import { themeOptions } from "theme/mui-theme-styles"
 import "./App.scss"
-import Container from "@mui/material/Container"
-import Typography from "@mui/material/Typography"
-import Box from "@mui/material/Box"
-import { Copyright } from "components/copyright"
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import { createBrowserRouter, RouterProvider, createHashRouter } from "react-router-dom"
 import { ProductFilterProvider } from "contexts/FilterProductContext"
 import ProductList from "components/ProductList/ProductList"
 import ProductDetails from "components/ProductDetails/ProductDetails"
-import { collections } from "./data/collection"
-import ReactJson from "react-json-view"
-import { product } from "./data/product"
 
 function App() {
   const [appIsReady, setAppIsReady] = useState(false)
@@ -20,7 +12,7 @@ function App() {
   useEffect(() => {
     setAppIsReady(true)
   }, [])
-  const router = createBrowserRouter([
+  const router = createHashRouter([
     {
       path: "/",
       element: <ProductList />,
